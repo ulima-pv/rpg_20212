4ae9c37b012dc04e2bd06454e0a80a20036bb6ea
Hernan <hernan@jsatch.com>
2021-10-28 21:09:48 -0500

    Sistema de menus terminado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public MenuManager menuManager;
 
     private float movementX;
     private float movementY;
@@ -14,6 +15,20 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         CanMove = true;
+        // Nos vamos a inscribir como observadores de MenuManager (OnMenuOpenEvent)
+        menuManager.OnMenuOpenEvent += MenuManager_OnMenuOpenEvent;
+        // Nos vamos a inscribir como observadores de MenuManager (OnMenuCloseEvent)
+        menuManager.OnMenuCloseEvent += MenuManager_OnMenuCloseEvent;
+    }
+
+    private void MenuManager_OnMenuCloseEvent(object sender, System.EventArgs e)
+    {
+        CanMove = true;
+    }
+
+    private void MenuManager_OnMenuOpenEvent(object sender, System.EventArgs e)
+    {
+        CanMove = false;
     }
 
     private void Update()
